0ff509e7110ac947408d5950aa0c8c8cd59975ce
online59 <taez.khamrangsi@gmail.com>
2023-12-14 00:42:52 +0700

    Added transaction container

--- lib/features/transaction/presentation/screens/transaction_item_container.dart
+++ lib/features/transaction/presentation/screens/transaction_item_container.dart
@@ -1,46 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:piggy/features/transaction/presentation/widgets/list_builder_widget.dart';
+import 'package:piggy/features/stickyheader/presentation/screens/sticky_header_list_page.dart';
 
-class TransactionContainer extends StatefulWidget {
+class TransactionContainer extends StatelessWidget {
   const TransactionContainer({super.key});
 
   @override
-  State<TransactionContainer> createState() => _TransactionContainerState();
-}
-
-class _TransactionContainerState extends State<TransactionContainer> {
-  bool isSelectionMode = false;
-  final int listLength = 30;
-  late List<bool> _selected;
-  bool _selectAll = false;
-  bool _isGridMode = false;
-
-  @override
-  void initState() {
-    super.initState();
-    initializeSelection();
-  }
-
-  void initializeSelection() {
-    _selected = List<bool>.generate(listLength, (_) => false);
-  }
-
-  @override
-  void dispose() {
-    _selected.clear();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return ListBuilderWidget(
-      isSelectionMode: isSelectionMode,
-      selectedList: _selected,
-      onSelectionChange: (bool x) {
-        setState(() {
-          isSelectionMode = x;
-        });
-      },
-    );
+    return const StickyHeaderListPage();
   }
 }
