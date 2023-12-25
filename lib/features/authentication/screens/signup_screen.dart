7209553e612535c68c516132fb097bffcbcba227
online59 <82426454+online59@users.noreply.github.com>
2023-12-25 09:41:15 +0700

    Add signup_screen.dart

---
+++ lib/features/authentication/screens/signup_screen.dart
@@ -0,0 +1,14 @@
+
+
+class SignupScreen extends StatelessWidget {
+	const SignupScreen({Key?: key}) : super(key:key);
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			body: SingleChildScrollView(
+			
+			),
+		);
+	}
+}
