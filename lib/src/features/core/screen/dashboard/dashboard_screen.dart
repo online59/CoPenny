b6b015b509d5bb3fecf177aaa57f621fbc0d1f5a
online59 <taez.khamrangsi@gmail.com>
2023-12-27 22:48:21 +0700

    Updates

--- lib/src/features/core/screen/dashboard/dashboard_screen.dart
+++ lib/src/features/core/screen/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:piggy/src/constants/sizes.dart';
 import 'package:piggy/src/constants/text_strings.dart';
 
 class DashboardScreen extends StatelessWidget {
@@ -6,31 +7,69 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    //dummy data
+    var items = List<String>.generate(30, (i) => 'Item $i');
+
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          color: Colors.grey[100],
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(mCurrentBalance.toUpperCase(),
-                  style: Theme.of(context).textTheme.bodySmall),
-              Text(mBalanceDummy,
-                  style: Theme.of(context).textTheme.bodySmall),
-              Text.rich(
-                TextSpan(
-                  text: "$mMonthDummy ",
-                  style: Theme.of(context).textTheme.bodySmall,
-                  children: [
+        body: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(mDefaultSize),
+              color: Theme.of(context).colorScheme.primary,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(mCurrentBalance.toUpperCase(),
+                      style: Theme.of(context).textTheme.bodySmall),
+                  const SizedBox(
+                    height: mVSpacingSmall,
+                  ),
+                  Text.rich(
                     TextSpan(
-                      text: mYearDummy,
+                      text: "$mThb ",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      children: [
+                        TextSpan(
+                            text: mBalanceDummy,
+                            style: Theme.of(context).textTheme.headlineLarge)
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: mVSpacingSmall,
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      text: "$mMonthDummy ",
                       style: Theme.of(context).textTheme.bodySmall,
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                      children: [
+                        TextSpan(
+                          text: mYearDummy,
+                          style: Theme.of(context).textTheme.bodySmall,
+                        )
+                      ],
+                    ),
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text.rich(TextSpan(text: mIncome, children: [
+                        TextSpan(text: "\n $mThb $mIncomeDummy")
+                      ])),
+                      Text.rich(TextSpan(text: mExpense, children: [
+                        TextSpan(text: "\n $mThb $mExpenseDummy")
+                      ])),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+            )
+          ],
         ),
       ),
     );
