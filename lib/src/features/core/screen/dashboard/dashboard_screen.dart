73d60234d8bd977a2acdb3acf4a5c865e7a18aba
online59 <taez.khamrangsi@gmail.com>
2024-01-03 19:29:52 +0700

    Updates

--- lib/src/features/core/screen/dashboard/dashboard_screen.dart
+++ lib/src/features/core/screen/dashboard/dashboard_screen.dart
@@ -8,42 +8,9 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: CustomScrollView(
-          slivers: [
-            const SliverPinnedAppBarWidget(
-              expandedHeight: 250.0,
-            ),
-            SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    color: Colors.teal[100 * index],
-                    child: Text("Grid item $index"),
-                  );
-                },
-                childCount: 20,
-              ),
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                maxCrossAxisExtent: 200.0,
-                mainAxisSpacing: 10.0,
-                crossAxisSpacing: 10.0,
-                childAspectRatio: 4.0,
-              ),
-            ),
-            SliverFixedExtentList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return Container(
-                    alignment: Alignment.center,
-                    color: Colors.lightBlue[100 * index],
-                    child: Text("List item $index"),
-                  );
-                },
-                childCount: 20,
-              ),
-              itemExtent: 50.0,
-            )
+        body: ListView(
+          children: [
+
           ],
         ),
       ),
